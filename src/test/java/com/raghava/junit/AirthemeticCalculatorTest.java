b59dd27f88b922eadbec753438e4ac874a928e24
raghavachatharajupalli <rch572@gmail.com>
2017-12-29 19:44:42 +0530

    Added failed test scenario

--- src/test/java/com/raghava/junit/AirthemeticCalculatorTest.java
+++ src/test/java/com/raghava/junit/AirthemeticCalculatorTest.java
@@ -1,15 +1,29 @@
 package com.raghava.junit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class AirthemeticCalculatorTest {
+	private AirthemeticCalculator calculator;
+
+	@Before
+	public void setup() {
+		calculator = new AirthemeticCalculator();
+
+	}
+
+	@After
+	public void tearDown() {
+		calculator = null;
+	}
 
 	@Test
 	public void testAdd() {
-		int expected=5;
-		AirthemeticCalculator calculator=new AirthemeticCalculator();
+		int expected = 5;
 		int actual = calculator.add(2, 3);
 		assertNotNull(actual);
 		assertEquals(expected, actual);
@@ -17,11 +31,15 @@ public class AirthemeticCalculatorTest {
 
 	@Test
 	public void testDivide() {
-		int expected=1;
-		AirthemeticCalculator calculator=new AirthemeticCalculator();
+		int expected = 1;
 		int actual = calculator.divide(3, 2);
 		assertNotNull(actual);
 		assertEquals(expected, actual);
+	}
+
+	@Test(expected = ArithmeticException.class)
+	public void divideShoudFailWithExceptions() {
+		calculator.divide(3, 0);
 	}
 
 }
